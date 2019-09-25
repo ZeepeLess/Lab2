fb792fcdbffe87affef0966f225625abae22038d
MaxPlay217 <artem200192@gmail.com>
2019-09-25 11:09:46 +0300

    comlete

---
+++ script.js
@@ -0,0 +1,11 @@
+let You_name = parseInt(prompt('Введите число'));
+let You_age = parseInt(prompt('Введите второе число'));
+let User_rights = confirm('Выполнить сложение');
+
+if ((User_rights === true) ) {
+
+   alert (You_name + You_age);
+}
+else{alert ("error");
+
+}
